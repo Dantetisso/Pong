17d2d005f127ac9e92ad33c62eecfac5004c9831
Dantetisso <dantetisoriero@gmail.com>
2025-09-30 21:15:43 -0300

    Movimiento pelota

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,43 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class BallController : MonoBehaviour
+public class BallController: MonoBehaviour
 {
     [SerializeField] private float speed;
-    Rigidbody2D rb;
-    Vector2 dir;
+    private Vector2 dir = Vector2.left;
+    private Vector2 startPos;
 
-// NO USAR FUERZAS PARA MOVER
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        dir = transform.position;
+        startPos = transform.position;
     }
 
-    void Update()
+    private void Update()
     {
-        //transform.Translate(dir.x , dir.y, 0);
+        transform.Translate(dir * speed * Time.deltaTime);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    public Vector2 ModifyDir(Vector2 direction)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            //rebota player
-            dir = new Vector2(1 * speed * Time.deltaTime, 0);
-        }
+       dir = new Vector2(dir.x,direction.y).normalized;
+       return dir;
+    }
 
-        if (collision.gameObject.CompareTag("Limit"))
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            // rebota
+            dir = Vector2.Reflect(dir,-dir).normalized;
         }
-
-        if (collision.gameObject.CompareTag("Arcs"))
+        
+        if (other.gameObject.CompareTag("Limit"))
         {
-            // suma punto
-
+            if (other.transform.position.y > transform.position.y)  // limite de arriba
+            {
+                dir = Vector2.Reflect(dir,Vector2.down).normalized;
+            }
+            else
+            {
+                dir = Vector2.Reflect(dir,Vector2.up).normalized;
+            }                                                       // limite de abajo
         }
+        
+        if (other.gameObject.CompareTag("Arcs"))
+        {
+            RestartPosition();
+        }
+    }
+
+    void RestartPosition()
+    {
+        transform.position = startPos;
+        dir = Random.Range(0, 2) == 0 ? Vector2.left : Vector2.right;   // respawn aleatorio
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private float inputY;
-    private Vector2 vector;
-
+    public BallController ball;
+    private Vector2 movDirection;
+    
     private void Update()
     {
         inputY = Input.GetAxisRaw("Vertical");
 
         transform.Translate(0, inputY * speed * Time.deltaTime, 0);
+        movDirection = new Vector2(0, inputY).normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ball.ModifyDir(movDirection);
+        }
     }
 }
