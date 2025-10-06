c48c82e571f12f393ef5c64e72a66ae06a264e7a
Dantetisso <dantetisoriero@gmail.com>
2025-10-05 21:50:49 -0300

    -

--- Assets/Main/Scripts/GameManager.cs
+++ Assets/Main/Scripts/GameManager.cs
@@ -4,16 +4,34 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] Transform ballSpawnPoint;
     private int greenPoits;
     private int redPoints;
     public event Action<int> OnGreenPointsChanged;
     public event Action<int> OnRedPointsChanged;
-    
+
+    private void Start()
+    {
+      //  StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[0].position, Quaternion.identity);
+            PhotonNetwork.Instantiate(ballPrefab.name, ballSpawnPoint.position, Quaternion.identity);
+        } 
+    }
+
     public void AddPointsGreen(int newPoints)
     {
-        greenPoits  += newPoints;
+        greenPoits += newPoints;
         OnGreenPointsChanged?.Invoke(greenPoits);
-        
+
         if (greenPoits >= 5)
         {
             Victory();
